fdcad55cec4d8e81de10eaf00b2f1b7384220d5b
Rodrigo Moreira <62709202+rodrigomoreir@users.noreply.github.com>
2022-07-20 22:21:43 -0300

    feat: add firebase request

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import {
   HStack,
   IconButton,
@@ -10,14 +12,18 @@ import {
   Center,
 } from "native-base";
 import { ChatTeardropText, SignOut } from "phosphor-react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Logo from "../assets/logo_secondary.svg";
 import { Button } from "../components/Button";
 import { Filter } from "../components/Filter";
 import { Order, OrderProps } from "../components/Order";
+import firestore from "@react-native-firebase/firestore";
+import { dateFormat } from "../utils/firestoreDateFormat";
+import { Loading } from "../components/Loading";
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
     "open"
   );
@@ -34,6 +40,39 @@ export function Home() {
     navigation.navigate("details", { orderId });
   };
 
+  const handleLogout = () => {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error);
+        return Alert.alert("Sair", "Não foi possível sair.");
+      });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const subscriber = firestore()
+      .collection("orders")
+      .where("status", "==", statusSelected)
+      .onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => {
+          const { patrimony, description, status, created_at } = doc.data();
+
+          return {
+            id: doc.id,
+            patrimony,
+            description,
+            status,
+            when: dateFormat(created_at),
+          };
+        });
+        setOrders(data);
+        setIsLoading(false);
+      });
+
+    return subscriber;
+  }, [statusSelected]);
+
   return (
     <VStack flex={1} pb={6} bg={"gray.700"}>
       <HStack
@@ -46,7 +85,10 @@ export function Home() {
         px={6}
       >
         <Logo />
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
       <VStack flex={1} px={6}>
         <HStack
@@ -73,30 +115,34 @@ export function Home() {
             isActive={statusSelected === "closed"}
           />
         </HStack>
-        <FlatList
-          data={orders}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
-          )}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 75 }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText color={colors.gray[300]} size={40} />
-              <Text
-                color={"gray.300"}
-                fontSize={"xl"}
-                mt={6}
-                textAlign="center"
-              >
-                Você ainda não possui {"\n"}
-                solicitações{" "}
-                {statusSelected === "open" ? "em andamento" : "finalizadas"}
-              </Text>
-            </Center>
-          )}
-        />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={orders}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+            )}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 75 }}
+            ListEmptyComponent={() => (
+              <Center>
+                <ChatTeardropText color={colors.gray[300]} size={40} />
+                <Text
+                  color={"gray.300"}
+                  fontSize={"xl"}
+                  mt={6}
+                  textAlign="center"
+                >
+                  Você ainda não possui {"\n"}
+                  solicitações{" "}
+                  {statusSelected === "open" ? "em andamento" : "finalizadas"}
+                </Text>
+              </Center>
+            )}
+          />
+        )}
 
         <Button title="Nova solicitação" onPress={handleNewOrder} />
       </VStack>
